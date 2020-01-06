32d4342ed6128dc580b8978f636c0577e7cf8222
djavier77 <djcollaguazoc@uce.edu.ec>
2020-01-06 02:42:33 -0500

    Prueba con sublime merge

--- captcha.py
+++ captcha.py
@@ -1,11 +1,11 @@
 try:
         from PIL import Image
 except ImportError:
-        import Image
+        import image_to_string
 import pytesseract
 
 pytesseract.pytesseract.tesseract_cmd = r'C:\Program Files\Tesseract-OCR\tesseract.exe'
-captcha = pytesseract.image_to_string(Image.open('ce.jpg'),config='-psm 8 -c tessedit_char_whitelist=0123456789')
+captcha = pytesseract.image_to_string(Image.open('ce2.jpg'),config='-psm 8 -c tessedit_char_whitelist=0123456789')
 captcha = captcha.replace(" ", "").strip()
 print(captcha)
-print(captcha)
+
