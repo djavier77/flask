bcbad303e04e58622adce4149afa90a4d1b2e924
djavier77 <djcollaguazoc@uce.edu.ec>
2020-01-06 02:32:28 -0500

    Primer cambio

--- captcha.py
+++ captcha.py
@@ -8,3 +8,4 @@ pytesseract.pytesseract.tesseract_cmd = r'C:\Program Files\Tesseract-OCR\tessera
 captcha = pytesseract.image_to_string(Image.open('ce.jpg'),config='-psm 8 -c tessedit_char_whitelist=0123456789')
 captcha = captcha.replace(" ", "").strip()
 print(captcha)
+print(captcha)
